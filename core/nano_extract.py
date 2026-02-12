1ba0d331bb43b41be36b040205d875a90dc512b4
Rufio Dinoto(Musk) <244thanva@gmail.com>
2026-02-12 22:10:39 +0700

    Update nano_extract.py

--- core/nano_extract.py
+++ core/nano_extract.py
@@ -1,1 +1,16 @@
+from PIL import Image
+import base64,json
+from cryptography.fernet import Fernet
 
+def extract(nano_image):
+    img = Image.open(nano_image)
+
+    encrypted = img.text["nano"]
+    key = img.text["key"]
+
+    f = Fernet(key.encode())
+    data = json.loads(f.decrypt(encrypted.encode()))
+
+    for name,content in data.items():
+        with open("data/out_"+name,"wb") as w:
+            w.write(base64.b64decode(content))
